690039e48d08e16a3c741e4b36226c2076534946
deniselupe <dgrodriguez24@hotmail.com>
2023-06-12 16:47:03 +0000

    style: version one of login page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,14 +1,22 @@
+import Link from 'next/link';
+import Logo from '../../components/svgs/seecho-logo.svg';
+
 export default function LoginPage() {
     return (
-        <div>
-            <form>
-                <label>Email or Username
-                    <input />
-                </label>
-                <label>Password
-                    <input />
-                </label>
-                <button>Log in</button>
+        <div className="container mx-auto h-full flex items-center">
+            <form className="w-2/5 h-3/4 min-w-[350px] mx-auto p-10 rounded-3xl flex flex-col items-center bg-seecho-darkblue">
+                <Link href="/" className="mb-10">
+                    <Logo className="w-64 h-14 text-seecho-gold hover:text-black" />
+                </Link>
+                <div className="w-full mb-4">
+                    <label className="text-seecho-gold text-2xl block mb-2" htmlFor="email">Email</label>
+                    <input className="block text-1xl leading-8 rounded w-full" type="email" id="email" name="name" />
+                </div>
+                <div className="w-full mb-16">
+                    <label className="text-seecho-gold text-2xl block mb-2" htmlFor="password">Password</label>
+                    <input className="block text-1xl leading-8 rounded w-full" type="text" id="password" name="password" />
+                </div>
+                <button className="text-2xl text-seecho-darkblue bg-seecho-gold hover:bg-seecho-lightblue rounded w-full">Log in</button>
             </form>
         </div>
     );
--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -17,14 +17,16 @@ export default function NavBar() {
         return (
             <nav className="mx-60 my-16 flex justify-between min-w-[600px]">
                 <Link href="/">
-                    <Logo className="w-64 h-14" />
+                    <Logo className="w-64 h-14 text-seecho-gold hover:text-black" />
                 </Link>
                 <div className="w-1/2 flex justify-end items-end">
                     <DiscordIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
                     <TwitterIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
                     <SearchIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
                     <MenuIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
-                    <p className="text-4xl font-medium text-seecho-gold hover:text-black ml-7">Login</p>
+                    <Link href="/login">
+                        <p className="text-4xl font-medium text-seecho-gold hover:text-black ml-7">Login</p>
+                    </Link>
                 </div>
             </nav>
         );
