bf12e9e8ddbf6f540d4286e6c1b8b6c244d61c6b
deniselupe <dgrodriguez24@hotmail.com>
2023-06-12 17:59:51 +0000

    style: login page version two

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,23 +1,27 @@
 import Link from 'next/link';
-import Logo from '../../components/svgs/seecho-logo.svg';
 
 export default function LoginPage() {
     return (
-        <div className="container mx-auto h-full flex items-center">
-            <form className="w-2/5 h-3/4 min-w-[350px] mx-auto p-10 rounded-3xl flex flex-col items-center bg-seecho-darkblue">
-                <Link href="/" className="mb-10">
-                    <Logo className="w-64 h-14 text-seecho-gold hover:text-black" />
-                </Link>
-                <div className="w-full mb-4">
-                    <label className="text-seecho-gold text-2xl block mb-2" htmlFor="email">Email</label>
-                    <input className="block text-1xl leading-8 rounded w-full" type="email" id="email" name="name" />
-                </div>
-                <div className="w-full mb-16">
-                    <label className="text-seecho-gold text-2xl block mb-2" htmlFor="password">Password</label>
-                    <input className="block text-1xl leading-8 rounded w-full" type="text" id="password" name="password" />
-                </div>
-                <button className="text-2xl text-seecho-darkblue bg-seecho-gold hover:bg-seecho-lightblue rounded w-full">Log in</button>
-            </form>
-        </div>
+        <form className="w-2/6 h-3/4 min-w-[400px] mx-auto mt-28 p-10 rounded-3xl flex flex-col items-center bg-seecho-darkblue shadow-xl">
+            <div className="w-full mb-4">
+                <h1 className="mb-2 text-5xl text-seecho-gold text-center">Welcome back!</h1>
+                <h2 className="text-seecho-gold text-center">We're so excited to see you again!</h2>
+            </div>
+            <div className="w-full mb-4">
+                <label className="mb-2 text-seecho-gold text-2xl block" htmlFor="email">Email</label>
+                <input className="w-full block text-1xl leading-8 rounded" type="email" id="email" name="name" />
+            </div>
+            <div className="w-full mb-2">
+                <label className="mb-2 text-seecho-gold text-2xl block" htmlFor="password">Password</label>
+                <input className="w-full block text-1xl leading-8 rounded" type="text" id="password" name="password" />
+            </div>
+            <Link href="#" className="w-full mb-12">
+                <p className="text-seecho-lightblue hover:underline">Forgot your password?</p>
+            </Link>
+            <button className="w-full mb-4 text-2xl text-seecho-darkblue bg-seecho-gold hover:bg-seecho-lightblue rounded">Log in</button>
+            <p className="w-full inline-flex text-seecho-gold">New to seecho?
+                <Link href="#" className="ml-1 text-seecho-lightblue hover:underline">Create an account</Link>
+            </p>
+        </form>
     );
 }
--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -11,24 +11,26 @@ import { useSelectedLayoutSegment } from 'next/navigation';
 export default function NavBar() {
     const segment = useSelectedLayoutSegment();
 
-    if (segment === 'login') {
-        return null;
-    } else {
-        return (
-            <nav className="mx-60 my-16 flex justify-between min-w-[600px]">
-                <Link href="/">
-                    <Logo className="w-64 h-14 text-seecho-gold hover:text-black" />
-                </Link>
-                <div className="w-1/2 flex justify-end items-end">
-                    <DiscordIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
-                    <TwitterIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
-                    <SearchIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
-                    <MenuIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
+    return (
+        <nav className="mx-60 my-8 flex justify-between min-w-[600px]">
+            <Link href="/">
+                <Logo className="w-64 h-14 text-seecho-gold hover:text-black" />
+            </Link>
+            <div className="w-1/2 flex justify-end items-end">
+                <DiscordIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
+                <TwitterIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
+                <SearchIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
+                <MenuIcon className="text-seecho-gold hover:text-black min-w-[40px] w-10 h-10 ml-7" />
+                {
+                    segment === 'login'
+                    ?
+                    null
+                    :
                     <Link href="/login">
                         <p className="text-4xl font-medium text-seecho-gold hover:text-black ml-7">Login</p>
                     </Link>
-                </div>
-            </nav>
-        );
-    }
+                }
+            </div>
+        </nav>
+    );
 }
