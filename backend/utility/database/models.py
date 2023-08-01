e156983e00cfebeccc9d2a87913de4a454be84c6
PancakePuncher <pancakepuncher@gmail.com>
2023-08-01 21:15:47 +0000

    feat: enrich "UserDetails" model

--- backend/utility/database/models.py
+++ backend/utility/database/models.py
@@ -5,3 +5,6 @@ class UserDetails(Model):
     uuid = fields.UUIDField(pk=True)
     email = fields.CharField(unique=True, max_length=255)
     password = fields.CharField(max_length=255)
+    created_on = fields.DatetimeField(auto_now_add=True)
+    last_modified = fields.DatetimeField(auto_now=True)
+    last_login = fields.DatetimeField(auto_now=False, auto_now_add=True)
