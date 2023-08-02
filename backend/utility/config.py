2cd227ac7346315e50f5a087286981eb204ed88f
PancakePuncher <pancakepuncher@gmail.com>
2023-08-02 21:42:51 +0000

    fix: fix format

--- backend/utility/config.py
+++ backend/utility/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     DATABASE: str = "DEFAULT"
     GEN_SCHEMAS: bool = "True"
     RELOAD: bool = "True"
-    UDS: str ="/tmp/uvicorn.sock"
+    UDS: str = "/tmp/uvicorn.sock"
     PROXY_HEADERS: bool = "True"
 
     model_config = SettingsConfigDict(env_file="../.env")
