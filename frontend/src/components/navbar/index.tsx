3bed0f8b05dca24e2f4692dcad4b557db42ef8d8
deniselupe <dgrodriguez24@hotmail.com>
2023-08-16 01:43:40 +0000

    build: create /chat route

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "@/public/svgs/seecho-logo.svg";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 export default function NavBar() {
+    const segment = useSelectedLayoutSegment();
+
+    if (segment === "chat") {
+        return null;
+    }
+
     return (
         <nav className="w-fit mx-auto my-8 sm:w-3/4 sm:flex sm:justify-between">
             <Link href="/" className="py-4 flex justify-center">
