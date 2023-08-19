355dbabc8ffabdf391083d3d12df125e16a749a9
deniselupe <dgrodriguez24@hotmail.com>
2023-08-19 01:47:33 +0000

    style: add transition css to chat sidebar

--- frontend/src/components/chat-sidebar/index.tsx
+++ frontend/src/components/chat-sidebar/index.tsx
@@ -15,14 +15,15 @@ export default function ChatSidebar() {
 
     console.log('open', open);
 
-    if (open) {
-        return (
-            <div className="w-64 h-screen flex flex-col px-8 text-white bg-zinc-900">
+    return (
+        <>
+            {!open && <SidebarToggle onClick={toggleSidebar} className="mt-6 ml-2 fixed z-2" />}
+            <div className={`w-60 h-screen flex flex-col px-4 text-white bg-zinc-900 ${open ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-200 ease-in-out z-1`}>
                 <div className="flex justify-between items-end mt-6">
                     <Link href="/">
                         <SeechoLogo className="w-32 text-seecho-gold hover:text-seecho-lightblue" />
                     </Link>
-                    <SidebarToggle onClick={toggleSidebar} />
+                    {open && <SidebarToggle onClick={toggleSidebar} />}
                 </div>
                 <ul className="mt-12">
                     <li>Dashboard</li>
@@ -40,8 +41,6 @@ export default function ChatSidebar() {
                     </ul>
                 </div>
             </div>
-        );
-    }
-
-    return <SidebarToggle onClick={toggleSidebar} />;
+        </>
+    );
 }
--- frontend/src/components/sidebar-toggle/index.tsx
+++ frontend/src/components/sidebar-toggle/index.tsx
@@ -2,8 +2,9 @@ import MenuIcon from '@/public/svgs/menu-icon.svg';
 
 type SidebarToggleType = {
     onClick: () => void;
+    className?: string;
 };
 
-export default function SidebarToggle({ onClick }: SidebarToggleType) {
-    return <MenuIcon onClick={onClick} className="w-8 text-seecho-gold border border-seecho-gold hover:border-seecho-lightblue rounded" />;
+export default function SidebarToggle({ onClick, className }: SidebarToggleType) {
+    return <MenuIcon onClick={onClick} className={`w-8 text-seecho-gold border border-seecho-gold hover:border-seecho-lightblue rounded ${className}`} />;
 }
