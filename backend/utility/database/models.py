e318e305faae1756539fb708965344d2781607f9
PancakePuncher <pancakepuncher@gmail.com>
2023-08-22 23:49:34 +0000

    feat: schema adjustments to prep for discord

--- backend/utility/database/models.py
+++ backend/utility/database/models.py
@@ -2,13 +2,27 @@ from tortoise import fields, Model
 
 
 class UserDetails(Model):
-    # CORE WEBSITE USER INFO
+    # CORE USER INFO
+    uuid = fields.UUIDField(pk=True)
+    public_uuid = fields.UUIDField()
+
+    # WEBSITE USER INFO
     uuid = fields.UUIDField(pk=True)
     email = fields.CharField(unique=True, max_length=30)
     password = fields.CharField(max_length=255)
     last_login = fields.DatetimeField(auto_now=False, auto_now_add=True)
 
+    # DISCORD USER INFO
+    discord_username = fields.CharField(unique=True, max_length=30)
+    discord_user_id = fields.IntField(unique=True, max_length=30)
+    # channel_id = fields.IntField()
+    # channel_topic = fields.IntField()
+    # channel_user = fields.IntField()
+
+
+
     # SECONDARY USER INFO
     account_created_on = fields.DatetimeField(auto_now_add=True)
     account_creation_source = fields.CharField(max_length=25)
     last_password_change = fields.DatetimeField(auto_now=False, auto_now_add=True)
+    last_discord_message = fields.DatetimeField(auto_now=False, auto_now_add=True)
