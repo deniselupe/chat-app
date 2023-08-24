1cfed96754090a60f8ca179983344e70308d0904
deniselupe <dgrodriguez24@hotmail.com>
2023-08-24 15:29:04 +0000

    feat: conditionally route users back to /signin

--- frontend/src/app/landing/page.tsx
+++ frontend/src/app/landing/page.tsx
@@ -1,8 +1,24 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 export default function LandingPage() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const code = searchParams.get("code");
+    const state = searchParams.get("state");
+    const authState = localStorage.getItem("authState");
+
+    useEffect(() => {
+        if (!state || !code) {
+            console.log("No state or code provided from provider. Please retry login.");
+            return router.replace("/signin");
+        } else if (!authState) {
+            console.log("No authState present in localStorage. Please retry login.");
+            return router.replace("/signin");
+        }
+    }, []);
+
     return <div>Loading....</div>;
 }
