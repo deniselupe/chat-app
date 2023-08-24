baafe0b2976e3f024b2df8e73fadb4239cf58850
deniselupe <dgrodriguez24@hotmail.com>
2023-08-24 14:58:59 +0000

    feat: introduce /landing page for auth redirect

---
+++ frontend/src/app/landing/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+export default function LandingPage() {
+    return <div>Loading....</div>;
+}
