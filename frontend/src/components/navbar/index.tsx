5ba8ee83b6c74d4d0a05865d48fd5b7f90c9782d
deniselupe <dgrodriguez24@hotmail.com>
2023-09-09 19:00:48 +0000

    feat: sign in to Discord directly from navbar

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,33 +1,16 @@
-"use client";
-
 import Link from "next/link";
 import Logo from "@/public/svgs/seecho-logo.svg";
-import TwitterIcon from "@/public/svgs/twitter-icon.svg";
-import SearchIcon from "@/public/svgs/search-icon.svg";
-import MenuIcon from "@/public/svgs/menu-icon.svg";
-import { useSelectedLayoutSegment } from "next/navigation";
 
 export default function NavBar() {
-    const segment = useSelectedLayoutSegment();
-
     return (
         <nav className="w-fit mx-auto my-8 sm:w-3/4 sm:flex sm:justify-between">
             <Link href="/" className="py-4 flex justify-center">
-                <Logo className="w-64 min-w-[256px] h-14 min-h-[56px] text-seecho-gold hover:text-black" />
+                <Logo className="w-24 text-seecho-gold hover:text-black" />
             </Link>
             <div className="py-4 flex justify-center items-center">
-                <TwitterIcon className="text-seecho-gold hover:text-black w-8 h-8 mx-2 lg:mx-4" />
-                <SearchIcon className="text-seecho-gold hover:text-black w-8 h-8 mx-2 sm:w-10 sm:h-10 lg:mx-4" />
-                <MenuIcon className="text-seecho-gold hover:text-black w-8 h-8 mx-2 sm:w-10 sm:h-10 lg:mx-4" />
-                {
-                    segment === "signin"
-                    ?
-                    null
-                    :
-                    <Link href="/signin">
-                        <p className="text-4xl font-medium text-seecho-gold hover:text-black mx-2 lg:mx-4">Sign in</p>
-                    </Link>
-                }
+                <Link href="https://ptilol.com/api/auth/state?provider=discord&intent=signin" prefetch={false}>
+                    <p className="text-2xl font-medium text-seecho-gold hover:text-black mx-2 lg:mx-4">Sign in</p>
+                </Link>
             </div>
         </nav>
     );
