ce71f756ac1aa696a4584aeafe027ee474453996
deniselupe <dgrodriguez24@hotmail.com>
2023-09-09 19:42:58 +0000

    refactor: downsize seecho logo on chat sidebar

--- frontend/src/components/chat-sidebar/index.tsx
+++ frontend/src/components/chat-sidebar/index.tsx
@@ -13,7 +13,7 @@ export default function ChatSidebar({ open, onClick }: ChatSidebarType) {
             <div className={`fixed top-0 left-0 z-20 w-64 h-full px-4 text-white bg-zinc-900 duration-500 ${open ? 'translate-x-0' : '-translate-x-full'}`}>
                 <div className="flex justify-between items-end py-4">
                     <Link href="/">
-                        <SeechoLogo className="w-32 text-seecho-gold hover:text-seecho-lightblue" />
+                        <SeechoLogo className="w-24 text-seecho-gold hover:text-seecho-lightblue" />
                     </Link>
                     {open && <SidebarToggle onClick={onClick} />}
                 </div>
