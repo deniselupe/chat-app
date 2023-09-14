cddc2be572f65d75e0b2595b2a175e9f83d1c8b5
deniselupe <dgrodriguez24@hotmail.com>
2023-09-14 16:17:57 +0000

    feat: give /chat a optional dynamic route

---
+++ frontend/src/app/chat/[[...chatId]]/page.tsx
@@ -0,0 +1,24 @@
+import Messages from "@/components/messages";
+import SeechoInteraction from "@/components/seecho-interaction";
+import { notFound } from "next/navigation";
+
+interface ChatProps {
+    params: {
+        chatId?: string[];
+    }
+}
+
+export default async function Chat({ params }: ChatProps) {
+    const { chatId } = params;
+
+    if (!!chatId && chatId.length > 1) {
+        notFound();
+    }
+
+    return (
+        <div className="w-full h-full flex flex-col-reverse lg:flex-row">
+            <Messages />
+            <SeechoInteraction />
+        </div>
+    );
+}
