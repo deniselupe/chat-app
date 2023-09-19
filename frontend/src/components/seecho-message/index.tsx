633ec119d354cce4f5cff59750eb7a93e76ac675
deniselupe <dgrodriguez24@hotmail.com>
2023-09-19 18:22:03 +0000

    refactor: reverse styling for user and seecho msg

--- frontend/src/components/seecho-message/index.tsx
+++ frontend/src/components/seecho-message/index.tsx
@@ -5,12 +5,10 @@ type SeechoMessageType = {
 
 export default function SeechoMessage({ children, contd }: SeechoMessageType) {
     return (
-        <div className={`w-full h-fit px-4 ${!!contd ? 'mt-1' : 'mt-6'} mb-2 flex flex-col items-end`}>
-            <div className="pl-11">
-                {!contd && <p className="text-sm text-white text-right mb-1">Seecho</p>}
-                <div className="relative px-4 py-2 mr-4 text-md bg-seecho-lightgreen rounded-tl-2xl rounded-bl-2xl rounded-br-2xl before:absolute before:w-10 before:h-4 before:border before:border-transparent before:top-0 before:-right-10 before:rounded-tl-2xl before:shadow-seecho-bubble">
-                    {children}
-                </div>
+        <div className={`w-full h-fit px-4 ${!!contd ? 'mt-1' : 'mt-6'} mb-2`}>
+            {!contd && <p className="text-sm text-white mb-1">Seecho</p>}
+            <div className="relative w-fit px-4 py-2 ml-4 text-md bg-seecho-gold rounded-tr-2xl rounded-br-2xl rounded-bl-2xl before:absolute before:w-10 before:h-4 before:border before:border-transparent before:top-0 before:-left-10 before:rounded-tr-2xl before:shadow-user-bubble">
+                {children}
             </div>
         </div>
     );
