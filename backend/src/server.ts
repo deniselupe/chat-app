2033b14d7c04a2dc0da9b20f942c49abe56435f9
deniselupe <dgrodriguez24@hotmail.com>
2023-10-23 22:21:31 +0000

    build: move /auth endpoints to authRouter.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
-import express, { Request, Response, response } from "express";
-import { createServer, request } from "node:http";
+import helmet from "helmet";
+import express, { Request, Response } from "express";
+import { createServer } from "node:http";
+import authRouter from "./routers/authRouter";
 
 dotenv.config();
 
@@ -8,47 +10,11 @@ const PORT = process.env.PORT;
 const app = express();
 const server = createServer(app);
 
+app.use(helmet());
+app.use("/auth", authRouter);
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello from Express.js!");
-});
-
-app.get("/auth/user", async (req: Request, res: Response) => {
-    const code = req.query.code;
-    
-    if (code) {
-        try {
-            const tokenResponse = await fetch("https://discord.com/api/oauth2/token", {
-                method: "POST",
-                body: new URLSearchParams({
-                    client_id: process.env.DISCORD_CLIENT_ID,
-                    client_secret: process.env.DISCORD_SECRET,
-                    code: code.toString(),
-                    grant_type: "authorization_code",
-                    redirect_uri: "https://ptilol.com/api/auth/user",
-                    scope: "identify email",
-                }).toString(),
-                headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            });
-
-            const oAuthData = await tokenResponse.json();
-            console.log(oAuthData);
-
-            const tokenType = oAuthData.token_type;
-            const accessToken = oAuthData.access_token;
-
-            const userResult = await fetch("https://discord.com/api/users/@me", {
-                headers: { authorization: `${tokenType} ${accessToken}` },
-            });
-
-            const userResultData = await userResult.json();
-
-            console.log(userResultData);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    res.send("Check for TokenResponse");
 });
 
 server.listen(PORT, () => {
