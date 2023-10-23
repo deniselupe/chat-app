f14606d3ab84e62b4596b43602773e4fb4019b3e
deniselupe <dgrodriguez24@hotmail.com>
2023-10-23 19:59:30 +0000

    feat: request Discord OAuth access_token

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-import express, { Request, Response } from "express";
-import { createServer } from "node:http";
+import express, { Request, Response, response } from "express";
+import { createServer, request } from "node:http";
 
 dotenv.config();
 
@@ -10,6 +10,34 @@ const server = createServer(app);
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello from Express.js!");
+});
+
+app.get("/auth/user", async (req: Request, res: Response) => {
+    const code = req.query.code;
+    
+    if (code) {
+        try {
+            const tokenResponse = await fetch("https://discord.com/api/oauth2/token", {
+                method: "POST",
+                body: new URLSearchParams({
+                    client_id: process.env.DISCORD_CLIENT_ID,
+                    client_secret: process.env.DISCORD_SECRET,
+                    code: code.toString(),
+                    grant_type: "authorization_code",
+                    redirect_uri: "https://ptilol.com/api/auth/user",
+                    scope: "identify email",
+                }).toString(),
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            });
+
+            const oAuthData = await tokenResponse.json();
+            console.log(oAuthData);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    res.send("Check for TokenResponse");
 });
 
 server.listen(PORT, () => {
